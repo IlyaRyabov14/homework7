9cf920a671f9a38cd3873c3d3a106287bb397eb6
ilya <ilya-ryabov03@yandex.ru>
2022-10-26 14:28:14 +0300

    homework

---
+++ №50/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите номер строки: ");
+int row = int.Parse(Console.ReadLine()??"");
+Console.Write("Введите номер столбца: ");
+int columns = int.Parse(Console.ReadLine()??"");
+
+int [,] Array = new int[5, 10]; 
+ 
+void Fullarray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+Fullarray(Array);
+PrintArray(Array);
+if (row < Array.GetLength(0) && columns < Array.GetLength(1)) Console.WriteLine(Array[row, columns]);
+else Console.WriteLine($"{row} {columns} -> такого числа в массиве нет");
---
+++ №52/Program.cs
@@ -0,0 +1,36 @@
+﻿double [,] array = new double [5,10];
+void Fullarray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+Fullarray(array);
+PrintArray(array);
+double [] result = new double[10];
+double Nresult = 0;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Nresult += array[i, j];
+    }
+    Console.Write($"{ Nresult / 5 } ");
+}
